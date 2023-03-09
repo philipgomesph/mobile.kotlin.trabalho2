625748751a0965e5701240374e1ece2770cc914a
philipgomesph <philip.gomesph@gmail.com>
2023-03-09 19:31:21 -0300

    atividade 2 & 3: Finalizada atv 2 e inicio da atv3.

---
+++ app/src/main/java/com/pdm/trabalhojeffao2/Atividade3Activity.kt
@@ -0,0 +1,11 @@
+package com.pdm.trabalhojeffao2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Atividade3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atividade3)
+    }
+}
